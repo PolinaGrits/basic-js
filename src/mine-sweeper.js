1fcf00ccc0c8cb676df9c60c8c50ba67adfbca02
Polina <polinka256@gmail.com>
2022-05-01 17:08:45 +0300

    feat: mine-sweeper done

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function minesweeper(matrix) {
+  let mine = [];
+  for (let i = 0; i < matrix.length; i++) {
+    mine[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+      //слева и справа от элемента
+      if ((matrix[i][j-1] !== undefined) && (matrix[i][j-1] === true) || 
+          (matrix[i][j+1] !== undefined) && (matrix[i][j+1] === true))
+          ++count;
+      // проверка сверху 
+      if (Array.isArray(matrix[i-1])){
+        if ((matrix[i-1][j-1] !== undefined) && (matrix[i-1][j-1] === true) || 
+            (matrix[i-1][j] !== undefined) && (matrix[i-1][j] === true) || 
+            (matrix[i-1][j+1] !== undefined) && (matrix[i-1][j+1] === true))
+        ++count
+      }
+       // проверка снизу 
+      if (Array.isArray(matrix[i+1])){
+        if ((matrix[i+1][j-1] !== undefined) && (matrix[i+1][j-1] === true) || 
+            (matrix[i+1][j] !== undefined) && (matrix[i+1][j] === true) || 
+            (matrix[i+1][j+1] !== undefined) && (matrix[i+1][j+1] === true))
+        ++count
+      }
+      mine[i][j] = count;
+    }
+  }
+  return mine;
 }
+
+console.log(minesweeper([
+  [true, false, false],
+  [false, true, false],
+  [false, false, false]
+   ]))
+
+
+
 
 module.exports = {
   minesweeper
